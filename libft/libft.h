de9e96e3f86c310d036b7152e361c4c4fa807aca
Eliot Monachon <eliotmonachon@Eliots-MacBook-Air.local>
2024-10-07 10:16:36 +0200

    Added color schemes

---
+++ libft/libft.h
@@ -0,0 +1,8 @@
+#ifndef LIBFT_H		// Si LIBFT_H n'est pas encore défini
+#define LIBFT_H		// On le définit ici
+
+
+
+void hello();		// Déclaration de la fonction 
+
+#endif // LIBFT_H	// Fin de la protection d'inclusion
