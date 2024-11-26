5676b1eb3b8d140d412b0f2dcf18d74ca9e65c89
eriotto-momomo <eliot.monachon@proton.me>
2024-11-26 12:15:26 +0100

    bm

--- ft_printf/ft_printf_TESTS/printf_itoa_REWORKED.c
+++ ft_printf/ft_printf_TESTS/printf_itoa_REWORKED.c
@@ -6,7 +6,7 @@
 /*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 10:28:26 by emonacho          #+#    #+#             */
-/*   Updated: 2024/11/26 11:35:48 by emonacho         ###   ########.fr       */
+/*   Updated: 2024/11/26 11:57:32 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,21 @@ static int	count(int n)
 char		*itoa(int n)
 {
 	long	i;
-	int		longueur;
+	int		len;
 	char	*str;
 
 	i = n;
 	if (n == -2147483648)
 		write(1, "-2147483648", 12);
-	longueur = count(n);
-	if (!(str = (char *)malloc(sizeof(char) * longueur)))
+	len = count(n);
+	if (!(str = (char *)malloc(sizeof(char) * len)))
 		return (NULL);
 	if (n < 0)
 		n = n * (-1);
-	str[--longueur] = '\0';
-	while ((--longueur) >= 0)
+	str[--len] = '\0';
+	while ((--len) >= 0)
 	{
-		str[longueur] = n % 10 + '0';
+		str[len] = n % 10 + '0';
 		n = n / 10;
 	}
 	if (i < 0)
