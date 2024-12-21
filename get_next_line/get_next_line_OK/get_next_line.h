a2230e027bc2ec2e9f0201d86cf0a7755a12f579
Eliot Monachon <emonacho@c1r6s4.42lausanne.ch>
2024-12-21 16:53:42 +0100

    added corrected GNL

--- get_next_line/get_next_line_OK/get_next_line.h
+++ get_next_line/get_next_line_OK/get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emonacho <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 16:50:47 by emonacho          #+#    #+#             */
+/*   Updated: 2024/12/21 16:50:51 by emonacho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -5,7 +17,7 @@
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE = 42
+#  define BUFFER_SIZE = 42
 # endif
 
 char	*get_next_line(int fd);
--- get_next_line/get_next_line_TESTS/get_next_line.h
+++ get_next_line/get_next_line_TESTS/get_next_line.h
@@ -1,17 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emonacho <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 16:50:47 by emonacho          #+#    #+#             */
+/*   Updated: 2024/12/21 16:50:51 by emonacho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h> // TO DELETE!!!
-# include <fcntl.h> // TO DELETE!!!
-# include <string.h> // TO DELETE!!!
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE = 1
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE = 42
+# endif
 
-char 	*get_next_line(int fd);
+char	*get_next_line(int fd);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char *s1, char *s2);
