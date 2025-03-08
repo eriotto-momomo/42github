92802fc9baab13512edec80e7c1461efc5514825
eriotto-momomo <eliot.monachon@proton.me>
2025-03-08 18:22:04 +0100

    added minitalk

---
+++ R3/minitalk/minitalk.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 16:18:18 by emonacho          #+#    #+#             */
+/*   Updated: 2025/03/08 16:22:02 by emonacho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "./libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <stdbool.h>
+# include <stdlimits.h>
+
+
+
+#endif
---
+++ R3/minitalk/mt_client.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mt_client.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 16:18:08 by emonacho          #+#    #+#             */
+/*   Updated: 2025/03/08 18:08:35 by emonacho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 3)
+		exit(1); // Ou `perror`? Ou autre sortie d'erreur?
+		// Error! Command line arguments must be SEVRER_PID and message to send.
+
+
+
+}
