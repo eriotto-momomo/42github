cbdb6e1f0dbbe9608be510c6d59be59d95472626
Eliot Monachon <emonacho@c2r3s6.42lausanne.ch>
2025-08-29 14:20:48 +0200

    Refactoring monitoring

--- R4/philo/dinner/monitoring.c
+++ R4/philo/dinner/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 21:17:19 by emonacho          #+#    #+#             */
-/*   Updated: 2025/08/29 14:14:51 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/08/29 14:20:18 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,6 @@
 
 /*
 * Fonction give_priority et qui check et qui du coup fais wait les philos qui ont mange il y'a moins de 2 / tto_die*/
-
-//static bool philo_died(t_philo *p)
-//{
-//	handle_mutex(&p->s->monitor_lock, LOCK);
-//	if (get_time() >= p->starving_time)
-//	{
-//		handle_mutex(&p->s->monitor_lock, UNLOCK);
-//		print_philo(p, "died", true);
-//		//handle_mutex(&p->s->main_lock, LOCK);
-//		////*p->s->philo_died = true;
-//		//handle_mutex(&p->s->main_lock, UNLOCK);
-//		return (NULL);
-//	}
-//	handle_mutex(&p->s->monitor_lock, UNLOCK);
-//	return (false);
-//}
 
 static bool all_philos_are_full(t_main *s)
 {
@@ -41,6 +25,22 @@ static bool all_philos_are_full(t_main *s)
 		return (true);
 	}
 	handle_mutex(&s->monitor_lock, UNLOCK);
+	return (false);
+}
+
+static bool philo_died(t_philo *p)
+{
+	handle_mutex(&p->s->monitor_lock, LOCK);
+	if (get_time() >= p->starving_time)
+	{
+		handle_mutex(&p->s->monitor_lock, UNLOCK);
+		print_philo(p, "died", true);
+		handle_mutex(&p->s->main_lock, LOCK);
+		*p->s->philo_died = true;
+		handle_mutex(&p->s->main_lock, UNLOCK);
+		return (true);
+	}
+	handle_mutex(&p->s->monitor_lock, UNLOCK);
 	return (false);
 }
 
@@ -60,19 +60,8 @@ void	*waiter_routine(void *data)
 		{
 			if (all_philos_are_full(s) == true)
 				return (NULL);
-			//if (philo_died(&s->philos[i]) == true)
-			//	return (NULL);
-			handle_mutex(&s->monitor_lock, LOCK);
-			if (get_time() >= s->philos[i].starving_time)
-			{
-				handle_mutex(&s->monitor_lock, UNLOCK);
-				print_philo(&s->philos[i], "died", true);
-				handle_mutex(&s->main_lock, LOCK);
-				*s->philo_died = true;
-				handle_mutex(&s->main_lock, UNLOCK);
+			if (philo_died(&s->philos[i]) == true)
 				return (NULL);
-			}
-			handle_mutex(&s->monitor_lock, UNLOCK);
 			i++;
 		}
 		usleep(500);
