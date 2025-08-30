3508bdb2723eab7cd1241ecb0aef24bf4756eb13
Eliot Monachon <emonacho@c3r3s4.42lausanne.ch>
2025-08-30 13:39:09 +0200

    error handling in threads and monitoring done

--- R4/philo/dinner/monitoring.c
+++ R4/philo/dinner/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 21:17:19 by emonacho          #+#    #+#             */
-/*   Updated: 2025/08/29 22:35:54 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/08/30 13:38:37 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 static bool	all_philos_are_full(t_main *s)
 {
-	handle_mutex(&s->monitor_lock, LOCK);
+	if (handle_mutex(&s->monitor_lock, LOCK) != 0)
+		return (true);
 	if (s->philos_full == s->in[N_PHILO])
 	{
 		handle_mutex(&s->monitor_lock, UNLOCK);
 		return (true);
 	}
-	handle_mutex(&s->monitor_lock, UNLOCK);
+	if (handle_mutex(&s->monitor_lock, UNLOCK) != 0)
+		return (true);
 	return (false);
 }
 
@@ -28,14 +30,18 @@ static bool	philo_starved(t_philo *p)
 {
 	int	elapsed_time;
 
-	handle_mutex(&p->s->monitor_lock, LOCK);
-	elapsed_time = get_time_ms(p->last_meal); // error_handling
+	if (handle_mutex(&p->s->monitor_lock, LOCK) != 0)
+		return (true);
+	elapsed_time = get_time_ms(p->last_meal);
+	if (elapsed_time < 0)
+		return (true);
 	if (elapsed_time > p->tto_die)
 	{
 		handle_mutex(&p->s->monitor_lock, UNLOCK);
 		return (true);
 	}
-	handle_mutex(&p->s->monitor_lock, UNLOCK);
+	if (handle_mutex(&p->s->monitor_lock, UNLOCK) != 0)
+		return (true);
 	return (false);
 }
 
@@ -43,8 +49,10 @@ static bool	philo_died(t_philo *p)
 {
 	if (philo_starved(p))
 	{
-		print_philo(p, "died", true);
-		handle_mutex(&p->s->main_lock, LOCK);
+		if (print_philo(p, "died", true) != 0)
+			return (true);
+		if (handle_mutex(&p->s->main_lock, LOCK) != 0)
+			return (true);
 		*p->s->philo_died = true;
 		handle_mutex(&p->s->main_lock, UNLOCK);
 		return (true);
@@ -52,30 +60,40 @@ static bool	philo_died(t_philo *p)
 	return (false);
 }
 
+static int	check_philos(t_main *s)
+{
+	int	i;
+
+	i = -1;
+	while (++i < s->in[N_PHILO])
+	{
+		if (philo_died(&s->philos[i]) == true)
+			return (1);
+		if (all_philos_are_full(s) == true)
+			return (1);
+	}
+	return (0);
+}
+
 void	*waiter_routine(void *data)
 {
 	t_main	*s;
-	int		i;
 
 	s = (t_main *)data;
 	usleep(500);
 	while (1)
 	{
-		handle_mutex(&s->main_lock, LOCK);
+		if (handle_mutex(&s->main_lock, LOCK) != 0)
+			return (NULL);
 		if (*s->philo_died == true)
 		{
 			handle_mutex(&s->main_lock, UNLOCK);
 			break ;
 		}
-		handle_mutex(&s->main_lock, UNLOCK);
-		i = -1;
-		while (++i < s->in[N_PHILO])
-		{
-			if (philo_died(&s->philos[i]) == true)
-				return (NULL);
-			if (all_philos_are_full(s) == true)
-				return (NULL);
-		}
+		if (handle_mutex(&s->main_lock, UNLOCK) != 0)
+			return (NULL);
+		if (check_philos(s) != 0)
+			return (NULL);
 		usleep(500);
 	}
 	return (NULL);
--- R4/philo/dinner/philos.c
+++ R4/philo/dinner/philos.c
@@ -6,31 +6,32 @@
 /*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 15:20:59 by emonacho          #+#    #+#             */
-/*   Updated: 2025/08/30 12:24:08 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/08/30 13:37:45 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-static void	philo_wait(t_philo *p, int tto_wait)
+static int	philo_wait(t_philo *p, int tto_wait)
 {
 	struct timeval	now;
 	int				elapsed_time;
 
 	elapsed_time = 0;
 	if (gettimeofday(&now, NULL) != 0)
-		return ;// error_handling
+		return (1);
 	while (elapsed_time < tto_wait)
 	{
 		elapsed_time = get_time_ms(now);
 		if (elapsed_time < 0)
-			return ;
+			return (1);
 		if (elapsed_time > tto_wait)
 			break ;
 		if (dinner_is_done(p) == 1)
-			return ;
+			return (1);
 		usleep(500);
 	}
+	return (0);
 }
 
 int	philo_think(t_philo *p)
@@ -73,13 +74,15 @@ int	philo_eat(t_philo *p)
 
 int	print_philo(t_philo *p, char *status, bool end_dinner)
 {
-	t_time	now;
+	int	now;  //now == %d a la place de %llu
 
 	now = get_time_ms(p->s->ref_time); // error_handling
+	if (now < 0)
+		return (1);
 	handle_mutex(&p->s->main_lock, LOCK);
 	if (end_dinner == true)
 	{
-		printf("%llu %d %s\n", now, p->id, status);
+		printf("%d %d %s\n", now, p->id, status);  //now == %d a la place de %llu
 		handle_mutex(&p->s->main_lock, UNLOCK);
 		return (0);
 	}
@@ -88,7 +91,7 @@ int	print_philo(t_philo *p, char *status, bool end_dinner)
 		handle_mutex(&p->s->main_lock, UNLOCK);
 		return (0);
 	}
-	printf("%llu %d %s\n", now, p->id, status);
+	printf("%d %d %s\n", now, p->id, status); //now == %d a la place de %llu
 	handle_mutex(&p->s->main_lock, UNLOCK);
 	return (0);
 }
